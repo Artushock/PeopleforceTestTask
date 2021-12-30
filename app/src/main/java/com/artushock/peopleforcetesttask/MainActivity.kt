657ae7623a3e7bf49f7999c3950ee717625f5cca
Artiom Ponomariov <artushock@gmail.com>
2021-12-30 11:12:05 +0200

    Fixed problem with notification to API 26+

--- app/src/main/java/com/artushock/peopleforcetesttask/MainActivity.kt
+++ app/src/main/java/com/artushock/peopleforcetesttask/MainActivity.kt
@@ -1,10 +1,12 @@
 package com.artushock.peopleforcetesttask
 
+import android.app.NotificationChannel
 import android.app.NotificationManager
 import android.app.PendingIntent
+import android.content.Context
 import android.content.Intent
+import android.os.Build
 import android.os.Bundle
-import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.app.NotificationCompat
 import androidx.fragment.app.Fragment
@@ -102,21 +104,49 @@ class MainActivity : AppCompatActivity() {
         val intent = Intent(this, MainActivity::class.java)
         intent.putExtra(PAGER_POSITION, number - 1)
         intent.putExtra(AMOUNT_OF_FRAGMENTS, fragmentsCounter)
+
         val pendingIntent: PendingIntent =
             PendingIntent.getActivity(
                 this,
                 0,
                 intent,
-                PendingIntent.FLAG_UPDATE_CURRENT)
+                getPendingIntentFlag())
+
 
         val builder = NotificationCompat.Builder(this, CHANNEL_ID)
             .setSmallIcon(R.drawable.ic_baseline_circle_notifications)
             .setContentText("Notification $number")
             .setContentIntent(pendingIntent)
             .setAutoCancel(true)
+            .setPriority(NotificationCompat.PRIORITY_DEFAULT)
 
-        val notificationManager = getSystemService(NOTIFICATION_SERVICE) as NotificationManager
+        val notificationManager: NotificationManager =
+            getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+
+        createNotificationChanel(notificationManager)
+
         notificationManager.notify(number, builder.build())
+    }
+
+    private fun getPendingIntentFlag(): Int {
+        val flag = if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            PendingIntent.FLAG_IMMUTABLE or PendingIntent.FLAG_UPDATE_CURRENT
+        } else {
+            PendingIntent.FLAG_UPDATE_CURRENT
+        }
+        return flag
+    }
+
+    private fun createNotificationChanel(manager: NotificationManager) {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            val name = CHANNEL_ID
+            val descriptionText = "Chanel id for API 26+"
+            val importance = NotificationManager.IMPORTANCE_DEFAULT
+            val channel = NotificationChannel(CHANNEL_ID, name, importance).apply {
+                description = descriptionText
+            }
+            manager.createNotificationChannel(channel)
+        }
     }
 
     private fun removeNotification(number: Int) {
